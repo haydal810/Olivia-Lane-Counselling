aaecb23a621e01b31ad2580019b625a5830a3fd1
David <haydal.tmps@gmail.com>
2020-05-04 19:05:43 +0100

    fix to sendEmailJS.js file

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,5 +1,5 @@
 function sendMail(contact) {
-    emailjs.send("gmail", " ", {
+    emailjs.send("gmail", "olivia_lane_counselling_website", {
         "message": contact.message.value,
         "from_name": contact.name.value,
         "from_email": contact.emailaddress.value,
@@ -9,7 +9,7 @@ function sendMail(contact) {
             function (response) {
                 console.log("SUCCESS", response);
                 setTimeout("window.location.href = 'Thank_you_for_your_message.html';", 250); // the user will know that their message was sent successfully and is given feedback.
-                document.getElementById("contactForm").reset() // the form is reset after submit button is hit
+                document.getElementById("client_form").reset() // the form is reset after submit button is hit
             },
             function (error) {
                 console.log("FAILED", error);
