7b78763d4b14772ddbe3c4ab054ddc13e4054fde
David <haydal.tmps@gmail.com>
2020-05-04 19:43:01 +0100

    fix to bug on js file for emailJS

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,15 +1,15 @@
 function sendMail(contact_form) {
     emailjs.send("gmail", "olivia_lane_counselling_website", {
-        "message": contact.message.value,
-        "from_name": contact.name.value,
-        "from_email": contact.emailaddress.value,
-        "from_phone_number": contact.phone.value,
+        "message": contact_form.message.value,
+        "from_name": contact_form.name.value,
+        "from_email": contact_form.emailaddress.value,
+        "from_phone_number": contact_form.phone.value,
     })
         .then(
             function (response) {
                 console.log("SUCCESS", response);
                 setTimeout("window.location.href = 'Thank_you_for_your_message.html';", 250); // the user will know that their message was sent successfully and is given feedback.
-                document.getElementById("contactForm").reset() // the form is reset after submit button is hit
+                document.getElementById("contact_form").reset() // the form is reset after submit button is hit
             },
             function (error) {
                 console.log("FAILED", error);
